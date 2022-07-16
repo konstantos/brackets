e642cd53a6ca5e0b7b75ff8131a0fb3cf5786804
konstantos <sunnyday-23@mail.ru>
2022-07-16 13:25:04 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, conf) {
+
+  if (str.length % 2 != 0) {
+    return false;
+  }  
+
+  let st = str;
+  let cnt = 0;  
+
+  while (st.length != 0) {
+      for (let i = 0; i < conf.length; i++) {
+          if (st.includes(conf[i].join(""))) {
+              st = st.split(conf[i].join("")).join("");
+              cnt++;
+          } 
+      }
+
+      if (cnt == 0) return false  
+      else cnt = 0;
+  }
+
+  return true;
 }
